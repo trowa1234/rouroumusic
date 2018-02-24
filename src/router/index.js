e7b73291182f0fe7e26981b18ef0560bfc4fca32
kidd.lee <trowa1234@qq.com>
2018-02-24 14:29:58 +0800

    使用路由懒加载

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,71 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Recommend from '@/components/recommend/recommend'
-import Singer from '@/components/singer/singer'
-import Rank from '@/components/rank/rank'
-import Search from '@/components/search/search'
-import SingerDetail from '@/components/singer-detail/singer-detail'
-import Disc from '@/components/disc/disc'
-import TopList from '@/components/top-list/top-list'
-import UserCenter from '@/components/user-center/user-center'
+
+/* 项目较大时推荐使用路由懒加载来引入组件
+//import Recommend from '@/components/recommend/recommend'
+//import Singer from '@/components/singer/singer'
+//import Rank from '@/components/rank/rank'
+//import Search from '@/components/search/search'
+//import SingerDetail from '@/components/singer-detail/singer-detail'
+//import Disc from '@/components/disc/disc'
+//import TopList from '@/components/top-list/top-list'
+//import UserCenter from '@/components/user-center/user-center' 
+*/
 
 Vue.use(Router)
 
+//使用路由懒加载，引入组件
+const Recommend = (resolve) => {
+    import('@/components/recommend/recommend').then((recommend) => {
+        resolve(recommend)
+    })
+}
+
+const Singer = (resolve) => {
+    import('@/components/singer/singer').then((singer) => {
+        resolve(singer)
+    })
+}
+
+const Rank = (resolve) => {
+    import('@/components/rank/rank').then((rank) => {
+        resolve(rank)
+    })
+}
+
+const Search = (resolve) => {
+    import('@/components/search/search').then((search) => {
+        resolve(search)
+    })
+}
+
+const SingerDetail = (resolve) => {
+    import('@/components/singer-detail/singer-detail').then((singerDetail) => {
+        resolve(singerDetail)
+    })
+}
+
+const Disc = (resolve) => {
+    import('@/components/disc/disc').then((disc) => {
+        resolve(disc)
+    })
+}
+
+const TopList = (resolve) => {
+    import('@/components/top-list/top-list').then((topList) => {
+        resolve(topList)
+    })
+}
+
+const UserCenter = (resolve) => {
+    import('@/components/user-center/user-center').then((userCenter) => {
+        resolve(userCenter)
+    })
+}
+
+
+
+//配置路由
 export default new Router({
     routes: [{
         path: '/', //根路径
